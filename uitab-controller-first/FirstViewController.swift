bd47bcf24d3d7f5b85a2eaeab5eeaae4c9b959c1
Peter Zaporowski <peter@Peters-Mac.local>
2017-01-18 22:50:44 +0100

    Initial Commit

---
+++ uitab-controller-first/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  uitab-controller-first
+//
+//  Created by Peter Zaporowski on 18.01.2017.
+//  Copyright © 2017 Peter Zaporowski. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ uitab-controller-first/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  uitab-controller-first
+//
+//  Created by Peter Zaporowski on 18.01.2017.
+//  Copyright © 2017 Peter Zaporowski. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
